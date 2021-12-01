a86ab644b0eb26b793d375c5f92de60ec5a24bde
Armirola Roseller <armirolaroseller@gmail.com>
2021-12-01 17:29:01 +0800

    EDITED .html, .css, .js FILES, ADDED MORE PROJECTS for portfolio.html

--- scripts/index-script.js
+++ scripts/index-script.js
@@ -1,17 +1,19 @@
 /*--Webpage loading transition script--*/
 window.addEventListener('DOMContentLoaded', () => {
    document.body.classList.remove('fade-out');
- });
-
-/*--Contact submit button shows thank you message--*/
-var nextStep = document.querySelector('#nextStep');
-
-nextStep.addEventListener('click', function ("contact-form-submit") {
-  e.preventDefault();
-  // Hide first view
-  document.getElementById('my_form').style.display = 'none';
-
-  // Show thank you message element
-  document.getElementById('thank_you').style.display = 'block';
 });
 
+/*--Thank you message upon submitting form--*/
+function revealMessage(event){
+   document.getElementById("thank-you").style.display='block';
+}
+
+/*--Delays page reload upon submitting form--*/
+$("form").submit(function(e){
+   var form = this;
+   e.preventDefault();
+
+   setTimeout(function(){
+      form.submit();
+   }, 1500);
+});
--- scripts/portfolio-script.js
+++ scripts/portfolio-script.js
@@ -1,4 +1,35 @@
 /*--Webpage loading transition script--*/
 window.addEventListener('DOMContentLoaded', () => {
-   document.body.classList.remove('fade-out');
- });
+  document.body.classList.remove('fade-out');
+});
+
+/*--Projects section radio button project pop-ups--*/
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function currentSlide(n){
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n){
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+
+  if (n > slides.length) 
+    slideIndex = 1;
+
+  if (n < 1)
+    slideIndex = slides.length;
+
+  for (i = 0; i < slides.length; i++){
+      slides[i].style.display = "none";  
+  }
+
+  for (i = 0; i < dots.length; i++){
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
